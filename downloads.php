c07605aa18dc09ae1f9826e6dc6ee2e715b2900e
James Sutton <james.sutton@uk.ibm.com>
2016-06-23 09:30:36 +0100

    Updating Java client version
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

--- downloads.php
+++ downloads.php
@@ -21,13 +21,13 @@
              <tbody>
                  <tr>
                     <th scope="row">Java</th>
-                    <td>1.0.2 - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-releases/org/eclipse/paho/org.eclipse.paho.client.mqttv3/1.0.2/">Eclipse</a> / <a target="_blank" href="http://search.maven.org/#search%7Cga%7C1%7Ca%3A%22org.eclipse.paho.client.mqttv3%22">Maven Central</a></td>
+                    <td>1.1.0 - <a target="_blank" href="http://search.maven.org/#search%7Cga%7C1%7Ca%3A%22org.eclipse.paho.client.mqttv3%22">Maven Central</a></td>
                     <td>1.0.3-SNAPSHOT - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-snapshots/org/eclipse/paho/org.eclipse.paho.client.mqttv3/1.0.3-SNAPSHOT/">Eclipse</a></td>
                     <td><a target="_blank" href="https://github.com/eclipse/paho.mqtt.java">https://github.com/eclipse/paho.mqtt.java</a></td>
                 </tr>
                 <tr>
                     <th scope="row">Python</th>
-                    <td>1.1 - <a target="_blank" href="https://pypi.python.org/pypi/paho-mqtt/1.1">Pipy (Pip)</a></td>
+                    <td>1.1 - <a target="_blank" href="https://pypi.python.org/pypi/paho-mqtt/1.1">Pypi (Pip)</a></td>
                     <td><a target="_blank" href="https://github.com/eclipse/paho.mqtt.python/tree/develop"><i>Build from Develop branch Source</i></a></td>
                     <td><a target="_blank" href="https://github.com/eclipse/paho.mqtt.python">https://github.com/eclipse/paho.mqtt.python</a></td>
                 </tr>
