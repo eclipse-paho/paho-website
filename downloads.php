fb8e9149a1a2ee5933ed08364b72fb9fdd1a4511
James Sutton <james.sutton@uk.ibm.com>
2016-06-30 10:45:42 +0100

    Updating Android & Java download links
    Signed-off-by: James Sutton <james.sutton@uk.ibm.com>

--- downloads.php
+++ downloads.php
@@ -22,7 +22,7 @@
                  <tr>
                     <th scope="row">Java</th>
                     <td>1.1.0 - <a target="_blank" href="http://search.maven.org/#search%7Cga%7C1%7Ca%3A%22org.eclipse.paho.client.mqttv3%22">Maven Central</a></td>
-                    <td>1.0.3-SNAPSHOT - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-snapshots/org/eclipse/paho/org.eclipse.paho.client.mqttv3/1.0.3-SNAPSHOT/">Eclipse</a></td>
+                    <td>1.1.1-SNAPSHOT - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-snapshots/org/eclipse/paho/org.eclipse.paho.client.mqttv3/1.1.1-SNAPSHOT/">Eclipse</a></td>
                     <td><a target="_blank" href="https://github.com/eclipse/paho.mqtt.java">https://github.com/eclipse/paho.mqtt.java</a></td>
                 </tr>
                 <tr>
@@ -61,8 +61,8 @@
               </tr>
               <tr>
                  <th scope="row">Android Service</th>
-                 <td>1.0.2 - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-releases/org/eclipse/paho/org.eclipse.paho.android.service/1.0.2/">Eclipse</a></td>
-                 <td>1.0.3-SNAPSHOT - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-snapshots/org/eclipse/paho/org.eclipse.paho.android.service/1.0.3-SNAPSHOT/">Eclipse</a></td>
+                 <td>1.1.0 - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-releases/org/eclipse/paho/org.eclipse.paho.android.service/1.1.0/">Eclipse</a></td>
+                 <td>1.1.1-SNAPSHOT - <a target="_blank" href="https://repo.eclipse.org/content/repositories/paho-snapshots/org/eclipse/paho/org.eclipse.paho.android.service/1.1.1-SNAPSHOT/">Eclipse</a></td>
                  <td><a target="_blank" href="https://github.com/eclipse/paho.mqtt.android">https://github.com/eclipse/paho.mqtt.android</a></td>
              </tr>
              <tr>
