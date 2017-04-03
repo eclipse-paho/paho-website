d978d08b6f368149b404bf01e9d0140e3677aae5
Ian Craggs <icraggs@uk.ibm.com>
2017-04-03 15:58:02 +0100

    Update online C client doc

---
+++ files/mqttdoc/MQTTClient/html/search/searchdata.js
@@ -0,0 +1,36 @@
+var indexSectionsWithContent =
+{
+  0: "acdekmnpqrstuvw",
+  1: "m",
+  2: "m",
+  3: "m",
+  4: "cdekmnpqrstuvw",
+  5: "mp",
+  6: "m",
+  7: "amqst"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "files",
+  3: "functions",
+  4: "variables",
+  5: "typedefs",
+  6: "defines",
+  7: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Data Structures",
+  2: "Files",
+  3: "Functions",
+  4: "Variables",
+  5: "Typedefs",
+  6: "Macros",
+  7: "Pages"
+};
+
