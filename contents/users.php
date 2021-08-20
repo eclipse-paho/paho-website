e2e5b7e6566da55631c4173e92cc014b551bebf7
Ian Craggs <icraggs@gmail.com>
2021-08-20 16:51:30 +0100

    Add Github details to the users page

--- contents/users.php
+++ contents/users.php
@@ -3,6 +3,11 @@
 <div class="jumbotron jumbotron-fluid">
 <h2 style="color:black">Getting Involved</h2>
 
+<p>The main means of getting involved is to use Github, issues, pull requests and documentation. The set of Paho Github repositories can be found with this query: <a href="https://github.com/orgs/eclipse/repositories?language=&q=paho&sort=&type=">https://github.com/orgs/eclipse/repositories?language=&q=paho&sort=&type=</a>. </p>
+
+<p>More details about all the components, including about the Github repositories, can be found on the <a href="https://index.php?page=downloads/index.php">downloads</a> page.</p>
+
+
 <ul style="color:black">
 <li><a href="https://projects.eclipse.org/projects/iot.paho/who">Development team</a></li>
 <li><a href="https://accounts.eclipse.org/mailing-list/paho-dev">Paho mailing list</a></li>
